cfe77d585f25d4949968359ed1a9b375aac6763b
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-05-26 15:10:23 -0400

    added shopping list functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const generateListButton = document.querySelector('#generateList');
+const inputs = document.querySelectorAll('.input');
+const list = document.querySelector('#myList');
+
+generateListButton.addEventListener('click', function () {
+
+    inputs.forEach(function(item) {
+        const listItem = document.createElement('p');
+        listItem.innerText = item.value;
+        list.append(listItem);
+    });
+
+});
